fe5d9cc01114e8bb2d828db9b8ef3351839d5d69
Flinten <ckflintrup@gmail.com>
2023-10-09 14:35:16 +0200

    remove comp

--- scope-name/4-habitats/theme-provider/theme-provider.composition.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider } from './theme-provider';
-
-export const BasicThemeProvider = () => {
-  return (
-    <>
-      <ThemeProvider Theme="Almbrand">hello world!</ThemeProvider>
-    </>
-  );
-};
