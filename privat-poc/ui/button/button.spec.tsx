350bfa4ea338e3428cf0a78862c908f6f2337ee3
Flinten <ckflintrup@gmail.com>
2023-10-10 11:44:16 +0200

    test

--- privat-poc/ui/button/button.spec.tsx
+++ privat-poc/ui/button/button.spec.tsx
@@ -4,6 +4,6 @@ import { BasicButton } from './button.composition';
 
 it('renders with the correct text', () => {
   const { getByText } = render(<BasicButton />);
-  const rendered = getByText('This is a button2');
+  const rendered = getByText('This is a button23');
   expect(rendered).toBeTruthy();
 });
