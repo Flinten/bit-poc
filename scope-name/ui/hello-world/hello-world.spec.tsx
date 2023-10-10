c8bf6243d716bd3f12fa36affda0e3c8978a144f
Flinten <ckflintrup@gmail.com>
2023-10-10 13:43:02 +0200

    removed comp

--- scope-name/ui/hello-world/hello-world.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { BasicHelloWorld } from "./hello-world.composition";
-
-it('should render with the correct text', () => {
-    const { getByText } = render(<BasicHelloWorld />);
-    const rendered = getByText('Hello world!');
-    expect(rendered).toBeTruthy();
-});
-
